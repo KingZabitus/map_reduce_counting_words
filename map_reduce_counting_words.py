ff35df4d32ee39b07c011b3b337db889846c06a3
Marcos Paulo <103538721+KingZabitus@users.noreply.github.com>
2024-11-05 21:50:45 -0300

    add: reduce

--- map_reduce_counting_words.py
+++ map_reduce_counting_words.py
@@ -2,16 +2,15 @@ from collections import defaultdict
 from pathlib import Path
 from threading import Thread
 
-# Verifica se n]ao é um diretório e se a extensão do arquivo é txt
 def is_valid_file(file):
     return file.is_file() and file.name.endswith("txt")
 
 def map(file: Path):
     if is_valid_file(file):
         words = file.read_text().replace('\n', ' ').split(' ')
-        with Path('./output.tmp').open('a') as file:
+        with Path('./output.tmp').open('a') as output_file:
             for word in words:
-                file.write(f'{word}: "1"\n')
+                output_file.write(f'{word}: "1"\n')
 
 def order_temp_file():
     temp_file = Path('./output.tmp')
@@ -28,26 +27,38 @@ def order_temp_file():
 
     return word_ocurrence_list
 
-def reduce():
-    ...
+def reduce(word: str, occurrences: list[str]):
+    occurrences_count = len(occurrences)
+    print({'word': word, 'occurrences': occurrences_count})
 
 if __name__ == '__main__':
     path = Path('./')
     files_on_path = path.glob('*')
 
-    threads = []
+    map_threads = []
 
+    # Executa a função map em uma thread para cada arquivo
     for file in files_on_path:
         thread = Thread(target=map, args=(file,))
         thread.start()
-        threads.append(thread)
+        map_threads.append(thread)
 
-    # Aguarda que todas as threads terminem antes de chamar a função reduce()
-    for thread in threads:
+    # Aguarda todas as threads do map terminarem antes de chamar a função reduce
+    for thread in map_threads:
         thread.join()
 
+    # Ordena o arquivo temporário e obtém a lista de ocorrências de palavras
     word_ocurrence_list = order_temp_file()
 
-    print(*word_ocurrence_list, sep='\n')
+    reduce_threads = []
 
-    reduce()
+    # Executa a função em uma Thread para cada palavra junto de suas ocorrências
+    for word_ocurrence in word_ocurrence_list:
+        word = word_ocurrence.get('word')
+        occurrences = word_ocurrence.get('occurrences')
+        thread = Thread(target=reduce, args=(word, occurrences))
+        thread.start()
+        reduce_threads.append(thread)
+
+    for thread in reduce_threads:
+        thread.join()
